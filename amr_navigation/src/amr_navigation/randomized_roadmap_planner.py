3a6cd348f18514ca19f07033d37f8cb3b5cfb2a7
fmorillo <fernando.m@gmx.de>
2014-06-14 17:28:39 +0200

    Randomized Roadmap Planner Implemented

--- amr_navigation/src/amr_navigation/randomized_roadmap_planner.py
+++ amr_navigation/src/amr_navigation/randomized_roadmap_planner.py
@@ -1,32 +1,12 @@
 #!/usr/bin/env python
 
-
-#=============================== YOUR CODE HERE ===============================
-# Instructions: complete the currently empty RandomizedRoadmapPlanner class.
-#               An instance of this class will be created by the path_planner
-#               node. It should maintain a graph of points and the connections
-#               between them.
-#               The 'plan()' function should find a path between the requested
-#               points, inserting new nodes and edges if necessary. Make sure
-#               that it stops at some point in time if no path between the
-#               points exists.
-#
-# Remark: it will be necessary to test points and line segments for emptiness.
-#         The class is (as usual) ROS-independent, so the actual mechanism of
-#         performing these tests is abstracted by two callback functions, which
-#         the object receives during the construction. In order to test whether
-#         e.g. the point (1, 4) is free you should do:
-#
-#             free = self.point_free_cb((1, 4))
-#
-# Hint: use the standard function 'math.uniform()' to generate the coordinates
-#       for random points.
-#
-# Hint: if you decided to use 'pygraph' library for graph and search
-#       implementations, make sure that the graph object is stored in a member
-#       field called 'graph'. If this is the case, the nodes and edges of the
-#       graph will be automatically visualized by the path_planner node after
-#       each planning request.
+import rospy
+from math import sqrt
+from random import uniform
+from pygraph.classes.graph import graph
+from pygraph.classes.exceptions import NodeUnreachable
+from pygraph.algorithms.heuristics.euclidean import euclidean
+from pygraph.algorithms.minmax import heuristic_search
 
 class RandomizedRoadmapPlanner:
 
@@ -48,7 +28,13 @@ class RandomizedRoadmapPlanner:
         self.point_free_cb = point_free_cb
         self.line_free_cb = line_free_cb
         self.dimensions = dimensions
-
+        
+        # Instantiate graph, heuristic and maximum number of tries
+        self.graph = graph()
+        self.heuristic = euclidean()
+        self.max_tries = 50
+        pass
+        
     def plan(self, point1, point2):
         """
         Plan a path which connects the two given 2D points.
@@ -60,7 +46,74 @@ class RandomizedRoadmapPlanner:
         the end point. If the planning algorithm failed the returned list
         should be empty.
         """
-        return list()
+        path_to_target = list()
+        found_path_to_target = False
+        
+        # Saves start and end identifiers
+        start_identifier = len(self.graph.nodes())
+        end_identifier = start_identifier + 1
+        
+        #Check if points are free
+        if self.point_free_cb(point1) and self.point_free_cb(point2):
+            self.graph.add_node(start_identifier, attrs=[('position', point1)])
+            self.graph.add_node(end_identifier, attrs=[('position', point2)])
+            # Checks if start and end point can be connected
+            if self.line_free_cb(point1, point2):
+                self.graph.add_edge((start_identifier, end_identifier), wt = self.distance(point1, point2))
+            # Find edges to the rest of the graph
+            for node_identifier, attr in self.graph.node_attr.iteritems():
+                position = attr[0][1]
+                if point1 != position and point2 != position: # if point and position is the same, line_free does not return and hangs
+                    if self.line_free_cb(point1, position):
+                        self.graph.add_edge((start_identifier, node_identifier), wt = self.distance(point1, position)) 
+                    if self.line_free_cb(point2, position):
+                        self.graph.add_edge((end_identifier, node_identifier), wt = self.distance(point2, position))
+        else:
+            # Stops if start or end point are not free
+            rospy.logwarn("Start or End Point are not free")
+            return path_to_target
+        
+        # Searches for a path and if there is none, a random point is created and added to the graph
+        count_tries = 0
+        while not found_path_to_target:
+            try:
+                # Check if there is a path
+                self.heuristic.optimize(self.graph)
+                identifier_path = heuristic_search(self.graph, start_identifier, end_identifier, self.heuristic)
+                found_path_to_target = True
+                rospy.logwarn("Found a Path")
+                # Resolve identfier and push them into path_to_target
+                for identifier in identifier_path:
+                    node_pose = self.graph.node_attributes(identifier)
+                    path_to_target.append(node_pose[0][1])                    
+            except NodeUnreachable:
+                # Create a new random point 
+                random_point_x = uniform(self.dimensions[0][0],self.dimensions[0][1])
+                random_point_y = uniform(self.dimensions[1][0],self.dimensions[1][1])
+                random_point = (random_point_x, random_point_y)
+                # If new random point is free add it as node to the graph
+                if self.point_free_cb(random_point):
+                    identifier = len(self.graph.nodes())
+                    self.graph.add_node(identifier, attrs=[('position', random_point)])
+                    # Check if new random point can be connected to any other point and add edges
+                    for node_identifier, attr in self.graph.node_attr.iteritems():
+                        position = attr[0][1]
+                        if random_point != position:
+                            if self.line_free_cb(random_point, position):
+                                self.graph.add_edge((identifier, node_identifier), wt = self.distance(random_point, position))                                
+            # Check if max tries are exceeded and stop
+            count_tries += 1
+            if count_tries >= self.max_tries:
+                rospy.logwarn("Maximum tries exceeded, no Path could be found")
+                break
+                                
+        return path_to_target
+
+    def distance(self, point1, point2):
+        '''
+        Calculates the distance between two points.
+        '''
+        return sqrt(pow(point1[0] - point2[0], 2) + pow(point1[1] - point2[1], 2))
 
     def remove_edge(self, point1, point2):
         """
